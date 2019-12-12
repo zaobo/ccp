da6583b9026f6a097779c18e21ee04a86c52413f
zhangaobo <zhangab1113@163.com>
2019-12-12 12:03:01 +0800

    add annoations

---
+++ HighConcurrent/hccp_concurrency/src/main/java/com/zab/hccpexample/annoations/NotThreadSafe.java
@@ -0,0 +1,15 @@
+package com.zab.hccpexample.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记线程不安全的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+    String value() default "";
+}
